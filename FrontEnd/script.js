72a3291086303734dd7b4b9f4fcce2620403f249
anthonymackiw@outlook.fr <94121080+Popo30240@users.noreply.github.com>
2025-02-25 15:18:44 +0100

    V 0.2 Add: de la fonction qui permet de générer des boutons. Et d'une fonction qu'au clique notre base de donnée soit générée sur la page HTML.

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,12 +1,63 @@
-/* Variables */
+/* Fonctions */
 
-const arrayCatAll =[]
+/**
+ * 
+ * @param {string} category : les catégories de la galerie
+ * On pointe sur le titre de la section portfolio
+ * On crée une balise button
+ * On ajoute le texte du bouton 
+ * genereButton(category);
+ */
 
-const arrayCatObjets =[]
+function genereButton(category) {
+  const buttonFiltrer = category;
+  const sectionPortfolio = document.querySelector(".titlePortfolio");
+  const buttonElement = document.createElement("button");
+  //buttonElement.className(buttonFiltrer);
+  buttonElement.innerText = buttonFiltrer;
+  sectionPortfolio.appendChild(buttonElement);
+}
+/*
+function filterByCategory(array, category) {
+  return array.filter(item => item.category.name === category);
+}
+*/
 
-const arrayCatApartments =[]
+/**
+ * 
+ * @param {string} array : les données de l'API
+ * On crée une balise figure
+ * * On crée une balise img
+ * On récupère l'url de l'image de la galerie
+ * On crée une balise figcaption
+ * On récupère le titre de la galerie
+ * On pointe sur la class gallery
+ * On ajoute la balise figure à la class gallery
+ * On ajoute la balise img et la balise figcaption à la balise figure
+ * On affiche les données de l'API dans
+ * la balise figure
+ */
 
-const arrayCatHotAndRes =[]
+function genereGallery(array) {        
+  const itemGallery = array;
+
+  const baliseElement = document.createElement("figure");
+
+  const imageElement = document.createElement("img");
+  imageElement.src = itemGallery.imageUrl;
+  //console.log("Image de la galerie", itemGallery.imageUrl);
+
+  const titleElement = document.createElement("figcaption");
+  titleElement.innerText = itemGallery.title;
+  //console.log("Titre de la galerie", itemGallery.title);
+
+  const classGallery = document.querySelector(".gallery");
+  classGallery.appendChild(baliseElement);
+
+  baliseElement.appendChild(imageElement);
+  baliseElement.appendChild(titleElement);
+  console.log("Données de l'API", itemGallery);
+}
 
 /**
  * Cette fonction permet de récupérer les données de l'API et de les filtrer par catégorie
@@ -24,34 +75,56 @@ async function fetchDataGallery() {
         throw new Error("Erreur réseau : " + response.status);
       }
       const dataGallery = await response.json();
-      console.log("Données de l'API", dataGallery);
+      //console.log("Données de l'API", dataGallery);
+
+      const boutonAll = document.querySelector("button");
+      boutonAll.addEventListener("click", function () {
+        for (let i = 0; i < dataGallery.length; i++) {
+          genereGallery(dataGallery[i]);
+        }
+      });
+
+      /*
+      for (let i = 0; i < dataGallery.length; i++) {
+        const Category = dataGallery[i].category.name;
+        console.log("type de la catégorie", Category);
+      }
+        */
+      /*
+      const Category = dataGallery[i].category.id;
+      console.log("type de la catégorie", Category);
+      genereButton(Category);
+
+      for (let i = 0; i < dataGallery.length; i++) {
+        genereButton(dataGallery[i].category.name);
+      }
+*/
+/*
       dataGallery.forEach(item => {
-        const id = item.category.id;
-        arrayCatAll.push(id);
-        //console.log("ID de la catégorie", id);
+        const typeCategory = item.category.name;
+        console.log("type de la catégorie", typeCategory);
         
-        switch (id) {
+        switch (typeCategory) {
           case 1:
-            arrayCatObjets.push(id);
+            genereButton(typeCategory);
             break;
           case 2:
-            arrayCatApartments.push(id);
+            genereButton(typeCategory);
             break;
           case 3:
-            arrayCatHotAndRes.push(id);
+            genereButton(typeCategory);
             break;
           default:
-            arrayCatAll.push(id);
             break;
         }
       });
-        console.log("Résultat des ID du tableau tous " + arrayCatAll);
-        console.log("Résultat des ID du tableau objet filtré " + arrayCatObjets);
-        console.log("Résultat des ID du tableau des appartements filtré " + arrayCatApartments);
-        console.log("Résultat des ID du tableau des Hôtels et Restaurants filtré " + arrayCatHotAndRes);
+*/
     } catch (error) {
       console.error("Erreur lors de la récupération des données :", error);
     }
   }
   
 fetchDataGallery();
+
+genereButton("tous");
+
